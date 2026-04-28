52686f4b3e71f710c41c57ad958fa6675a3453a5
Nitish Kumar <nk045729@gmail.com>
2026-04-28 23:01:14 +0530

    update

---
+++ demo/src/main/java/com/demo/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello from Servlet 🚀</h1>");
+    }
+}
